7d0e1a0bb530927f52b6d7f4b70a0bf89acfc1ad
Mohsen-Rahimi8001 <mohsenrahimi8001@gmail.com>
2023-05-04 22:41:47 +0330

    union function implemented

--- Q4Solution.py
+++ Q4Solution.py
@@ -56,21 +56,105 @@ def star(fa:VisualDFA|VisualNFA) -> dict:
     return res
     
 
-def union(fa1, fa2) -> dict:
-    pass
+def union(fa1:VisualDFA|VisualNFA, fa2:VisualDFA|VisualNFA) -> dict:
+    new_init = 'q0'
+    new_fi = 'q1'
+    
+    new_states = set()
+    for i in range(len(fa1.states)):
+        new_states.add(f"q{i}1")
+
+    for i in range(len(fa2.states)):
+        new_states.add(f"q{i}2")
+    
+
+    res = {
+    "states": new_states,
+    "input_symbols": set(fa1.input_symbols).union(fa2.input_symbols),
+    "transitions": dict(),
+    "final_states": str({new_fi})
+    }
+
+    res['states'].add(new_init)
+    res['initial_state'] = new_init
+    res['transitions'] = {
+        new_init: {
+            '': str(
+                    {
+                    fa1.initial_state+"1", fa2.initial_state+"2"
+                }
+            )
+        }
+    }
+
+    for state, value in fa1.transitions.items():
+        new_transition = dict()
+        for symbol, targetset in value.items():
+            new_transition[symbol] = set()
+            for target in targetset:
+                new_transition[symbol].add(target+"1")
+
+            if symbol != "" or state not in fa1.final_states:
+                new_transition[symbol] = str(new_transition[symbol])
+            else:
+                new_transition[symbol] = new_transition[symbol]
+        
+        res['transitions'][state+"1"] = new_transition
+
+    
+    for state, value in fa2.transitions.items():
+        new_transition = dict()
+        for symbol, targetset in value.items():
+            new_transition[symbol] = set()
+            for target in targetset:
+                new_transition[symbol].add(target+"2")
+            
+            if symbol != "" or state not in fa2.final_states:
+                new_transition[symbol] = str(new_transition[symbol])
+            else:
+                new_transition[symbol] = new_transition[symbol]
+
+        res['transitions'][state+"2"] = new_transition
+
+    for final in fa1.final_states:
+        new_final = final+"1"
+        if (res['transitions'].get(new_final)):
+            res['transitions'][new_final][""] = str({new_fi})
+        else:
+            res["transitions"][new_final] = {
+                "": str({new_fi})
+            }
+
+
+    for final in fa2.final_states:
+        new_final = final+"2"
+        if (res['transitions'].get(new_final)):
+            res['transitions'][new_final][""] = str({new_fi})
+        else:
+            res["transitions"][new_final] = {
+                "": str({new_fi})
+            }
+    
+    res["states"].add(new_fi)
+
+    res['input_symbols'] = str(res['input_symbols'])
+    res['states'] = str(res['states'])
+
+    return res
 
 def concatenation(fa1, fa2) -> dict:
     pass
 
 
-path = os.path.join('.', 'Packages', 'samples', 'phase4-sample', 'star', 'in', 'FA.json')
+path1 = os.path.join('.', 'Packages', 'samples', 'phase4-sample', 'union', 'in', 'FA1.json')
+path2 = os.path.join('.', 'Packages', 'samples', 'phase4-sample', 'union', 'in', 'FA2.json')
 
-fa = get_fa(path)
-if not fa:
-    raise Exception("The json file you provided is neither nfa nor dfa.")
+fa1 = get_fa(path1)
+fa2 = get_fa(path2)
+if not fa1 or not fa2:
+    raise Exception("Json files you provided are neither nfa nor dfa.")
 
-res = star(fa)
-
+res = union(fa1, fa2)
 
 import json
 
